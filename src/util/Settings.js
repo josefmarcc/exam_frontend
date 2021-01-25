62d31d293aadc9e5a9848e3fc1e4897e744dbae1
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2021-01-25 21:41:27 +0100

    Added Admin

--- src/util/Settings.js
+++ src/util/Settings.js
@@ -1,4 +1,4 @@
-//const SERVER_URL = "http://localhost:8080/jpareststarter/api/";
-const SERVER_URL = "https://segatos.com/tomcat/exam_backend-1.0.1/api/";
+const SERVER_URL = "http://localhost:8080/jpareststarter/api/";
+//const SERVER_URL = "https://segatos.com/tomcat/exam_backend-1.0.1/api/";
 
 export default SERVER_URL;
